ede5d4d4d0c20dd64ccfb15ce62cd4ebe6a79fb6
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-11-07 17:48:49 -0200

    Layout with CSS and GridPane for more freedom

--- src/z808/ui/BeautyFactory.java
+++ src/z808/ui/BeautyFactory.java
@@ -1,26 +1,24 @@
 package z808.ui;
 
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.BorderWidths;
-
 import javafx.scene.control.TextArea;
 
 public class BeautyFactory {
-  protected static Border generalBorder () {
-		return new Border ((new BorderStroke(Color.BLACK, 
-																				 BorderStrokeStyle.SOLID,
-																				 CornerRadii.EMPTY,
-																				 BorderWidths.DEFAULT)));
-	}
-
-	protected static TextArea ReadOnlyArea () {return ReadOnlyArea(null);}
-	protected static TextArea ReadOnlyArea (String txt) {
+	protected static TextArea CodeArea (String txt) {
 		TextArea tx = new TextArea(txt);
 		tx.setDisable(true);
+		tx.setStyle("-fx-background-color: antiquewhite;"
+								+ "-fx-border-style: solid;"
+								+ "-fx-border-width: 3px;"
+								+ "-fx-opacity: 1;"
+								+ "-fx-text-fill: black;"
+								+ "-fx-font-size: 20px;");
+		return tx;
+	}
+	protected static TextArea OutputArea (String txt) {
+		TextArea tx = new TextArea(txt);
+		tx.setDisable(true);
+		tx.setStyle("-fx-opacity: 1;"
+								+ "-fx-font-size: 20px;");
 		return tx;
 	}
 }
--- src/z808/ui/UIz808.java
+++ src/z808/ui/UIz808.java
@@ -1,67 +1,53 @@
 package z808.ui;
 
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Node;
 
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
 
 import javafx.scene.layout.Region;
 
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-
-import javafx.geometry.Pos;
 
 import util.NotImplementedException;
 
 import z808.ui.BeautyFactory;
 
 public class UIz808 extends Application {
-	private BorderPane mainPane;
+	private GridPane mainPane;
 	private Scene mainScene;
 
-	private Region top;
-	private Region bottom;
-	private Region center;
-	private Region right;
-	private Region left;
-
-	private Border border;
+	private Region toolBar;
+	private Region srcCode;
+	private Region innCode;
+	private Region regBank;
+	private Region outArea;
 
 	public static void main(String...args) throws NotImplementedException {
 		UIz808.launch(args);
 	}
 
 	public void start(Stage stage) {
-		stage.setTitle("z808 Simulator by UFPel");
+		stage.setTitle("The Marvelous z808 Simulator");
+		stage.setResizable(false);
 
-		this.border = BeautyFactory.generalBorder();
-		this.mainPane = new BorderPane();
-		this.mainPane.setMaxSize(1, 1);
-		// rootNode.setAlignment(Pos.CENTER);
+		this.mainPane = new GridPane();
 
-		this.mainScene = new Scene(this.mainPane, 500, 500);
+		this.toolBar = new Button("toolBar");
+		this.srcCode = BeautyFactory.CodeArea("srcCode");
+		this.innCode = BeautyFactory.CodeArea("innCode");
+		this.regBank = BeautyFactory.CodeArea("regBank");
+		this.outArea = BeautyFactory.OutputArea("outArea");
+
+		this.mainPane.add(toolBar, 0, 0, 1, 1);
+		this.mainPane.add(srcCode, 0, 1, 1, 1);
+		this.mainPane.add(innCode, 1, 1, 1, 1);
+		this.mainPane.add(regBank, 2, 1, 1, 1);
+		this.mainPane.add(outArea, 0, 2, 1, 1);
+		
+		this.mainScene = new Scene(this.mainPane, 1024, 768);
 		stage.setScene(mainScene);
-
-		this.top = new Button("Interactive buttons bar");
-		this.bottom = BeautyFactory.ReadOnlyArea("ReadOnly output text area");
-		this.center = BeautyFactory.ReadOnlyArea("Main code area");
-		this.left = BeautyFactory.ReadOnlyArea("Current internal code representation text area");
-		this.right = BeautyFactory.ReadOnlyArea("Registers bank");
-
-		this.mainPane.setTop(this.top);
-	  this.mainPane.setBottom(this.bottom);
-		this.center.setBorder(this.border);
-		this.mainPane.setCenter(this.center);
-		this.mainPane.setLeft(this.left);
-		this.mainPane.setRight(this.right);
-
-		// rootNode.getChildren().addAll(btnRun, btnExit, response);
 		stage.show();
 	}
 }
