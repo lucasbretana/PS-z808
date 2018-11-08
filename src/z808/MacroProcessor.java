e6ea04cf721448e5f861e5b1a89edb35d4a3d9e9
alanaschwendler <alanaschwendler27@gmail.com>
2018-11-08 19:51:50 -0200

    First commit, functions headers

--- src/z808/MacroProcessor.java
+++ src/z808/MacroProcessor.java
@@ -1,9 +1,35 @@
 package z808;
 
+import java.util.List;
+
 import util.NotImplementedException;
 
+import z808.command.Command;
+
+/*
+ * @author Alana Schwendler
+ */ 
 public class MacroProcessor {
   public MacroProcessor() throws NotImplementedException {
     throw new NotImplementedException("TODO");
+  }
+
+  public static List<Command> expansionMode () {
+    /*
+    new mCall()
+    call translator
+    look on both macro tables
+    return list<command>
+    */
+    return null;
+  }
+
+  public static List<Command> definitionMode() {
+    /*
+    new mDef() 
+    create macro definition table 
+    macro name table
+    */
+    return null;
   }
 }
