498f7ec2dc18902573aa4ede0e862103827ebe45
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-11-08 22:20:10 -0200

    Things are the expected size

--- src/z808/ui/BeautyFactory.java
+++ src/z808/ui/BeautyFactory.java
@@ -1,5 +1,6 @@
 package z808.ui;
 
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -14,9 +15,12 @@ public class BeautyFactory {
 
 	protected static TextArea CodeArea (String txt) {
 		TextArea tx = new TextArea(txt);
-		tx.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.6,
-									 BeautyFactory.SCREEN_HEIGHT * 0.7);
+		tx.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.5,
+									 BeautyFactory.SCREEN_HEIGHT * 0.85);
 		tx.setStyle("-fx-background-color: antiquewhite;"
+								+ "-fx-border-style: solid;"
+								+ "-fx-border-color: silver;"
+								+ "-fx-border-width: 2px;"
 								+ "-fx-font-size: 20px;");
 		return tx;
 	}
@@ -24,9 +28,12 @@ public class BeautyFactory {
 	protected static TextArea ReadArea (String txt) {
 		TextArea tx = new TextArea(txt);
 		tx.setDisable(true);
-		tx.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.2,
-									 BeautyFactory.SCREEN_HEIGHT * 0.7);
+		tx.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.3,
+									 BeautyFactory.SCREEN_HEIGHT * 0.85);
 		tx.setStyle("-fx-background-color: antiquewhite;"
+								+ "-fx-border-style: solid;"
+								+ "-fx-border-color: silver;"
+								+ "-fx-border-width: 2px;"
 								+ "-fx-opacity: 1;"
 								+ "-fx-font-size: 20px;");
 		return tx;
@@ -36,59 +43,62 @@ public class BeautyFactory {
 		TextArea tx = new TextArea(txt);
 		tx.setDisable(true);
 		tx.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.8,
-									 BeautyFactory.SCREEN_HEIGHT * 0.7);
+									 BeautyFactory.SCREEN_HEIGHT * 0.2);
 		tx.setStyle("-fx-opacity: 1;"
-								+ "-fx-text-fill: black;"
+								+ "-fx-border-style: solid;"
+								+ "-fx-border-color: silver;"
+								+ "-fx-border-width: 2px;"
 								+ "-fx-font-size: 20px;");
-		tx.setMaxHeight(0.5);
 		return tx;
 	}
 
-	protected static HBox MemoryArea (int size) {
+	protected static ScrollPane MemoryArea (int size) {
+		ScrollPane region = new ScrollPane();
 		HBox mem = new HBox(0);
 		VBox idx = new VBox(0);
 		VBox mm1 = new VBox(0);
 		VBox mm2 = new VBox(0);
-		VBox mm3 = new VBox(0);
-		mem.getChildren().addAll(idx, mm1, mm2, mm3);
-		mem.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.1,
-										BeautyFactory.SCREEN_HEIGHT * 0.475);
+		mem.getChildren().addAll(idx, mm1, mm2);
 		idx.getChildren().add(BeautyFactory.MemoryLabel("Address"));
 	  mm1.getChildren().add(BeautyFactory.MemoryLabel("+0"));
 	  mm2.getChildren().add(BeautyFactory.MemoryLabel("+1"));
-	  mm3.getChildren().add(BeautyFactory.MemoryLabel("+2"));
-		for (int i = 0; i < size/3; ++i) {
-			idx.getChildren().add(BeautyFactory.MemoryLabel(String.format("%04X", i*3)));
+		for (int i = 0; i < size/2; ++i) {
+			idx.getChildren().add(BeautyFactory.MemoryLabel(String.format("%04X", i*2)));
 			mm1.getChildren().add(BeautyFactory.MemoryEntry("313", i%2 == 0));
 			mm2.getChildren().add(BeautyFactory.MemoryEntry("313", i%2 != 0));
-			mm3.getChildren().add(BeautyFactory.MemoryEntry("313", i%2 == 0));
 		}
-		return mem;
+		region.setPrefSize(BeautyFactory.SCREEN_WIDTH  * 0.2,
+											 BeautyFactory.SCREEN_HEIGHT * 0.475);
+		region.setContent(mem);
+		return region;
 	}
 
 	protected static Label MemoryLabel(String v) {
 		Label l = new Label(v);
+		int fontSize = (v.length() > 4) ? 12 : 16;
 		l.setStyle("-fx-background-color: white;"
 							 + "-fx-text-align: center;"
 							 + "-fx-border-style: solid;"
 							 + "-fx-border-color: silver;"
 							 + "-fx-border-width: 2px;"
-							 + "-fx-font-size: 16px;");
+							 + "-fx-font-size: " + fontSize + "px;");
 		l.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-		l.setMinWidth(BeautyFactory.SCREEN_WIDTH  * 0.2 / 3);
+		l.setMinSize(62, 24);
+		l.setMaxSize(62, 24);
 		return l;
 	}
 
 	protected static Label MemoryEntry(String v, boolean blue) {
 		Label l = new Label(v);
-		l.setStyle("-fx-background-color: " + (blue ? "lightgray" : "lightsteelblue") + ";"
+		l.setStyle("-fx-background-color: " + (blue ? "lightsteelblue" : "lightgray") + ";"
 							 + "-fx-text-align: center;"
 							 + "-fx-border-style: solid;"
 							 + "-fx-border-color: silver;"
 							 + "-fx-border-width: 2px;"
 							 + "-fx-font-size: 16px;");
 		l.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-		l.setMinWidth(BeautyFactory.SCREEN_WIDTH  * 0.2 / 3);
+		l.setMinSize(62, 24);
+		l.setMaxSize(62, 24);
 		return l;
 	}
 }
