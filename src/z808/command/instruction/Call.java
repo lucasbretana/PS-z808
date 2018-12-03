cc30fdb40bf7a3970331361fa299f320fcee4845
Lucas E. Bretana <lucasbretana@gmail.com>
2018-12-03 16:24:15 -0200

    Fix for Call and Assembler should always check for symbols on external table

--- src/z808/command/instruction/Call.java
+++ src/z808/command/instruction/Call.java
@@ -1,5 +1,6 @@
 package z808.command.instruction;
 
+import z808.memory.Address;
 import z808.memory.Memory;
 import z808.command.instruction.Instruction;
 
@@ -9,23 +10,33 @@ import util.ExecutionException;
 public class Call extends Instruction {
 	public static final int OPCODE = 0xE80000;
 	public static final int SIZE   = 3;
-	
-	public Call () {
-		this(null);
-	}
 
-	public Call (String label) {
+	private Address arg = null;  // the linker will substitute the function name for an address that will be in some module's global symbol table
+	private String u_arg = null; // the function name
+
+	public Call (String label, String call) {
 		this.size = SIZE;
-		this.label = label;
-
 		this.code = OPCODE;
-		return;
+
+		this.label = label;
+		this.u_arg = call;
 	}
 
+	public Call (String call) {
+		this(null, call);
+	}
+
+	@Override
 	public void exec (Memory mem) throws NotImplementedException, ExecutionException {
 	}
 	
+	@Override
 	public boolean isDefined() {
-		return false;
+		return this.arg != null;
+	}
+
+	@Override
+	public String getUndefValue() {
+		return this.u_arg;
 	}
 }
